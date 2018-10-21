425d29820abc4040329a36535b726126404a469a
zandimna <shaoyu0209@tuta.io>
2018-10-21 08:47:30 +0800

    merge fixes

--- monitor.py
+++ monitor.py
@@ -9,14 +9,9 @@ def MouseMonitor():
         
 
     def on_scroll(x, y, dx, dy):
-<<<<<<< HEAD
         global la 
         if la < datetime.datetime.now():
             la = datetime.datetime.now() 
-=======
-        global s
-        s = 0
->>>>>>> master
     def on_click(x, y, button, pressed):
         global la 
         if la < datetime.datetime.now():
@@ -26,11 +21,7 @@ def MouseMonitor():
         #on_move=on_move,
         on_click=on_click,
         on_scroll=on_scroll) as listener:
-<<<<<<< HEAD
-        listener.join()
-=======
             listener.join()
->>>>>>> master
 
 def KeyboardMonitor():
     def on_press(key):
